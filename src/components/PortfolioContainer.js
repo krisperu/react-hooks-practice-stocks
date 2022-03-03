20a422c0697356565deb90bbd937bb9fef5927d7
Kristina Peru <krissy.germanov@gmail.com>
2022-03-02 21:49:35 -0700

    All Deliverables

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({ stocks, onRemoveStock }) {
+
+  const stockCompArr = stocks.map(stock => (
+  <Stock 
+    key={stock.id} 
+    id={stock.id}
+    name={stock.name} 
+    price={stock.price} 
+    onClickStock={onRemoveStock} 
+  />
+))
   return (
-    <div>
-      <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
-    </div>
-  );
+
+  <div>
+    <h2>My Portfolio</h2>
+    {stockCompArr}
+  </div>
+);
 }
 
 export default PortfolioContainer;
